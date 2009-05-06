d4b244a0f225fe5fb5df2d558b85c21183263cf6
Mark Holton <holtonma@Macintosh-69.local>
2009-05-06 16:52:24 -0700

    latest

--- test/unit/portfolio_item_test.rb
+++ test/unit/portfolio_item_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PortfolioItemTest < ActiveSupport::TestCase
 	
-  test "calculating the current value of a portfolio item is accurate" do
+  test "calculating the current value of a portfolio_item should be qty times price" do
 	  item = portfolio_items(:one)
 	  asset = Asset.find(item.asset_id)
 	  current_val_mills = item.qty * asset.current_price
