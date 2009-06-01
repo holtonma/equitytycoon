139f61b6d58fc0096546354b6e3f5aaeb922d551
Mark Holton <holtonma@Macintosh-69.local>
2009-05-31 21:13:28 -0700

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :portfolio_items
   map.resources :portfolios
   map.resources :users
-  map.resources :home
+  #map.resources :home
   
   map.connect ':controller/:action', :controller => 'home'
   map.connect ':controller/:action.:format', :controller => 'home'
