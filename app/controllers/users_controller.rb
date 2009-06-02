eb26dfd0766ca0c93fea92bead5fef41884808d4
Mark Holton <holtonma@Macintosh-69.local>
2009-06-01 20:28:54 -0700

    temporarily putting admin functions back into users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,26 @@ class UsersController < ApplicationController
 	  @user = User.find(params[:id])
 	end
 	
+	def edit
+		@user = User.find(params[:id])
+	end
 	
- 
+	def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        flash[:notice] = 'User was successfully updated.'
+        format.html { redirect_to(@user) }
+        format.xml { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+	
   def create
     logout_keeping_session!
     @user = User.new(params[:user])
--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,23 +1,26 @@
 class UserMailer < ActionMailer::Base
+	
+	SITE = "equitytycoon.heroku.com"
+	
   def signup_notification(user)
     setup_email(user)
     @subject    += 'Please activate your new account'
   
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
+    @body[:url]  = "http://#{SITE}/activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = "http://#{SITE}"
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
       @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @subject     = "#{SITE}"
       @sent_on     = Time.now
       @body[:user] = user
     end
