c75fb7273e4a245b06467a09ffbb64e53dd40a0a
Mark Holton <holtonma@Macintosh-69.local>
2009-06-02 19:49:17 -0700

    removing all the plugins

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
   map.resources :users
-
-  map.resource :session
-
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'users', :action => 'create'
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users
-
-  map.resource :session
+  # map.resource :session
+  # map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  # map.login '/login', :controller => 'sessions', :action => 'new'
+  # map.register '/register', :controller => 'users', :action => 'create'
+  # map.signup '/signup', :controller => 'users', :action => 'new'
+  # map.resources :users
 
   map.resources :asset_types
   map.resources :assets
